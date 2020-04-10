6b716717588037c3dfe0f1597d1c83aeec12d88a
amit baghel <amitbaghel5129@gmail.com>
2020-04-10 16:03:06 +0530

    Added one more Message

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -30,6 +30,13 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+  
+  @Test
+  public void greetShouldIncludeGreetingPhrase2() {
+    String someone = "World";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 
     @Test
   public void greetShouldIncludeGreetingMessage() {
